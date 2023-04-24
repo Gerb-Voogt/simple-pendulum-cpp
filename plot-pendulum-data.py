8c2d2ee7eba49a429dcfe5f5a2326244fe9e450b
unknown <gerbenvoogt2@hotmail.com>
2023-04-24 16:44:49 +0000

    Added some plotting

--- plot-pendulum-data.py
+++ plot-pendulum-data.py
@@ -5,26 +5,62 @@ import numpy as np
 
 
 def main():
-    data = pd.read_csv('data_fe.csv')
+    fe_data = pd.read_csv('./data_fe.csv')
+    rk4_data = pd.read_csv('./data_rk4.csv')
 
+    ## Plotting the Forward Euler Results
     fig, ax = plt.subplots(1, 2, figsize=(16, 5))
     time_plot = ax[0]
     phase_space = ax[1]
-    time_plot.plot(data['t'], data['theta'], label=r'$\theta$')
-    time_plot.plot(data['t'], data['theta_dot'], label=r'$\frac{d \theta}{dt}$')
-    time_plot.plot(data['t'], data['theta_ddot'], label=r'$\frac{d^2 \theta}{dt^2}$')
+    time_plot.plot(fe_data['t'], fe_data['theta'], label=r'$\theta$')
+    time_plot.plot(fe_data['t'], fe_data['theta_dot'], label=r'$\frac{d \theta}{dt}$')
     time_plot.set(title = 'Angle, angular velocity and angular acceleration vs time',
               xlabel = r'$t$')
     time_plot.legend()
     time_plot.grid()
 
-    phase_space.plot(data['theta'], data['theta_dot'])
+    phase_space.plot(fe_data['theta'], fe_data['theta_dot'])
     phase_space.set(title = 'Phase portrait',
               xlabel=r'$\theta$',
               ylabel = r'$\frac{d\theta}{dt}$')
     phase_space.grid()
+    fig.suptitle('Forward Euler Results', fontsize=16)
 
     plt.show()
+
+    ## Plotting the RK4 results
+    fig, ax = plt.subplots(1, 2, figsize=(16, 5))
+    time_plot = ax[0]
+    phase_space = ax[1]
+    time_plot.plot(rk4_data['t'], rk4_data['theta'], label=r'$\theta$')
+    time_plot.plot(rk4_data['t'], rk4_data['theta_dot'], label=r'$\frac{d \theta}{dt}$')
+    time_plot.set(title = 'Angle, angular velocity and angular acceleration vs time',
+              xlabel = r'$t$')
+    time_plot.legend()
+    time_plot.grid()
+
+    phase_space.plot(rk4_data['theta'], rk4_data['theta_dot'])
+    phase_space.set(title = 'Phase portrait',
+              xlabel=r'$\theta$',
+              ylabel = r'$\frac{d\theta}{dt}$')
+    phase_space.grid()
+    fig.suptitle('RK4 Results', fontsize=16)
+
+    plt.show()
+
+    ## Plotting the difference between Forward Euler and RK4
+    eps = np.abs(fe_data['theta'] - rk4_data['theta'])
+    eps_dot = np.abs(fe_data['theta_dot'] - rk4_data['theta_dot'])
+    time = fe_data['t']
+
+    plt.plot(time, eps, label=r'$\varepsilon_{\theta}$', color='red', linestyle='dashed')
+    plt.plot(time, eps_dot, label=r'$\varepsilon_{d\theta/dt}$', color='blue', linestyle='dashed')
+    plt.xlabel('Time')
+    plt.ylabel('Local Error')
+    plt.title('Local Error defined in terms of difference between FE and RK4 solution.')
+    plt.legend()
+    plt.show()
+
 
 
 if __name__ == "__main__":
